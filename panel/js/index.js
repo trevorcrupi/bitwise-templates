a0b0f825aef65e5a7398b8af2f8a382300ec3278
Trevor Crupi <trevor.crupi27@gmail.com>
2016-03-04 22:32:30 -0500

    Added Navbar and Hero Image

---
+++ panel/js/index.js
@@ -0,0 +1,19 @@
+/*
+	Main JavaScript Funtionality for Panels
+
+	Author: Trevor Crupi
+	Date: 2/29/15
+
+	Wrapped Panel into one JS Class
+*/
+
+var Panels = function()
+{
+	console.log("Object Instantiated.");
+}
+
+/*
+	Usage: 
+
+	var panel =  new Panels();
+*/
