2dc0d29568e188ea42925fd158633068a5d73f56
Trevor Crupi <trevor.crupi27@gmail.com>
2016-03-21 00:53:59 -0400

    Added JQuery Plugin for smoother scroll, fixed responsivenes

---
+++ panel/js/smoothAnchor.js
@@ -0,0 +1,33 @@
+/*
+	JQUery plugin for easy autoscrolling to divs
+
+	Author: Trevor Crupi
+	Date: 2/29/15
+*/
+
+(function( $ ) {
+
+	$.fn.smoothAnchor = function( options ) {
+
+		var opts = $.extend( {}, $.fn.smoothAnchor.defaults, options );
+
+		function getPosition(el) {
+		    // yay readability
+		    for (var lx=0, ly=0;
+		         el != null;
+		         lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
+		    return {x: lx,y: ly};
+		}
+
+		var anchor = this;
+		$(opts.animated).animate({
+        	scrollTop: getPosition($(anchor.attr('href'))[0]).y-opts.offset
+    	}, opts.speed);			
+	};
+	$.fn.smoothAnchor.defaults = {
+		animated: 'html, body',
+		speed: "2000",
+		offset: "0"
+	};
+
+}(jQuery));
